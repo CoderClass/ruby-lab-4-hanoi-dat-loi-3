209f59687769ec9ff94d1974e2535dbe8b58107d
Harry Nguyen <hamxvela@gmail.com>
2016-11-01 20:06:10 +0700

    milestone 2

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,22 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.where(room_id: params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.build message_params
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      flash[:error] = "Something is wrong here"
+      redirect_to room_messages_path(@room)
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:username, :content)
+  end
+end
