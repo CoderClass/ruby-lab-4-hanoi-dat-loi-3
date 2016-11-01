83c39705e0c0f22ff335b3b411fd30273f0224ca
Loi Tran <lvt15@my.fsu.edu>
2016-11-01 19:35:24 +0700

    scaffoled the app

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,24 @@
+class RoomsController < ApplicationController
+  def create
+    @room = Room.new room_params
+    if @room.save
+      flash[:success] = "You've made a room!"
+      redirect_to root_path
+    else
+      flash[:error] = "Unable to create room."
+      render 'new'
+    end
+  end
+
+  def show
+    @room = Room.find params[:id]
+  end
+  def new
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
